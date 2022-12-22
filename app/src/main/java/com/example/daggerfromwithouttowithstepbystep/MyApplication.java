67a0407b7f4eb59f51a395d0a79d9102f7cb07b6
Akashkroy69 <Akashkroy@hotmail.com>
2022-12-22 05:45:39 +0530

    Written about Getting Rid Of DependencyComponent Class

--- app/src/main/java/com/example/daggerfromwithouttowithstepbystep/MyApplication.java
+++ app/src/main/java/com/example/daggerfromwithouttowithstepbystep/MyApplication.java
@@ -14,6 +14,16 @@ public class MyApplication extends Application {
     public void onCreate() {
         super.onCreate();
 
-        DependencyComponent.inject(this);
+        //this Dependency Component was introduced in design approach 3 lecture-1, project stage 2. Now we want to use
+        //Dagger in place of Dependency Component. so we are commenting it out.
+        //we will delete the dependency component class and will comment out all the lines where the dependency class and
+        //its methods has been used. Maybe we will not delete it but we will move the dependency component class in
+        //deprecated package.
+        //As instance of ViewModel class MainViewModel was being created by Dependency component, so we will comment out every line
+        // where viewModel is being used. We will later bring them back when we completely introduce Dagger in our project and
+        // it creates instance of MainViewModel
+        //DependencyComponent.inject(this);
+
+
     }
 }
